352d50b05573869aadae5ab55d5b61d7b4952117
John Briggs <vernworldwide@gmail.com>
2016-04-07 13:59:20 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Starting Point.
+
+Modified version of https://github.com/scotch-io/gulp-start with livereload added in for good measures.
---
+++ gulpfile.js
@@ -0,0 +1,63 @@
+var gulp  = require('gulp'),
+    gutil = require('gulp-util')
+
+    jshint        = require('gulp-jshint'),
+    sass          = require('gulp-sass'),
+    concat        = require('gulp-concat'),
+    sourcemaps    = require('gulp-sourcemaps'),
+    livereload    = require('gulp-livereload'),
+    autoprefixer  = require('gulp-autoprefixer'),
+    uglify        = require('gulp-uglify'),
+
+    input  = {
+      'sass': 'source/scss/**/*.scss',
+      'javascript': 'source/javascript/**/*.js',
+      'vendorjs': 'public/assets/javascript/vendor/**/*.js'
+    },
+
+    output = {
+      'stylesheets': 'public/assets/stylesheets',
+      'javascript': 'public/assets/javascript'
+    };
+
+/* run the watch task when gulp is called without arguments */
+gulp.task('default', ['watch']);
+
+/* run javascript through jshint */
+gulp.task('jshint', function() {
+  return gulp.src(input.javascript)
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+/* compile scss files */
+gulp.task('build-css', function() {
+  return gulp.src('source/scss/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(output.stylesheets))
+    .pipe(livereload());
+});
+
+/* concat javascript files, minify if --type production */
+gulp.task('build-js', function() {
+  return gulp.src(input.javascript)
+    .pipe(sourcemaps.init())
+    .pipe(concat('bundle.js'))
+    //only uglify if gulp is ran with '--type production'
+    .pipe(gutil.env.type === 'production' ? uglify() : gutil.noop()) 
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(output.javascript));
+});
+
+/* Watch these files for changes and run the task on update */
+gulp.task('watch', function() {
+  livereload.listen();
+  gulp.watch(input.javascript, ['jshint', 'build-js']);
+  gulp.watch(input.sass, ['build-css']);
+});
